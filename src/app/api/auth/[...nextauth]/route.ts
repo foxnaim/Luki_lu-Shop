ba3fce561cd7bbe503355fc03f097ddbd0f88990
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-02 19:36:36 +0500

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/app/api/auth/[...nextauth]/route.ts 	deleted:    src/app/api/auth/register/register.ts 	deleted:    src/app/components/header/index.tsx 	deleted:    src/app/components/lib/auth.ts 	deleted:    src/app/components/lib/prisma.ts 	deleted:    src/app/prisma/schema.prisma 	modified:   yarn.lock

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { authOptions } from "@/app/components/lib/auth"; // Вынесем логику в `auth.ts`
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
